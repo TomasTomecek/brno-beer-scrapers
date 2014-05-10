5f3280689ef8cbfcb479c9e411891f9de28cc853
Tomas Tomecek <ttomecek@redhat.com>
2014-05-10 22:48:02 +0200

    finish op

--- web/op.js
+++ web/op.js
@@ -1,5 +1,6 @@
 var cheerio = require('cheerio');
 var request = require('request');
+var web = require('./web');
 var url = 'http://ochutnavkovapivnice.cz/prave_na_cepu/'
 
 var l = console.log;
@@ -7,30 +8,26 @@ var l = console.log;
 function scrape(html){
     var $ = cheerio.load(html);
 
-    l('Parser started.');
-
     result = []
 
     $('table tr').each(function(tr_index, tr) {
-        var tds = $(tr).find('td');
-        var beer = {
-            name: $(tds[0]).text(),
-            type: $(tds[1]).text(),
-            epm: $(tds[2]).text(),
-            abv: $(tds[3]).text(),
-            ipu: $(tds[4]).text(),
-            brewery: $(tds[5]).text(),
-            city: $(tds[6]).text(),
-            country: $(tds[7]).text(),
-        };
-        result.push(beer);
+        if (tr_index > 0) {
+            var tds = $(tr).find('td');
+            var beer = {
+                name: $(tds[0]).text(),
+                type: $(tds[1]).text(),
+                epm: $(tds[2]).text(),
+                abv: $(tds[3]).text(),
+                ipu: $(tds[4]).text(),
+                brewery: $(tds[5]).text(),
+                city: $(tds[6]).text(),
+                country: $(tds[7]).text(),
+            };
+            result.push(beer);
+        }
     });
-
-    l(result);
-    l('Parser ended.');
+    return result;
 }
-
-var start = Date.now();
 
 request({
     uri: url,
@@ -38,9 +35,8 @@ request({
         'User-Agent': 'Mozilla/5.0 (Windows NT 6.2; WOW64) AppleWebKit/537.4 (KHTML, like Gecko) Chrome/22.0.1229.94 Safari/537.4'
     }
 }, function (error, response, body) {
-    var req_time = Date.now() - start;
-    l('Request took ' + req_time + 'ms.');
     if (!error && response.statusCode == 200) {
-        scrape(body);
+        data = scrape(body);
+        web.post(data);
     }
 });
